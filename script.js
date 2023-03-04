eb6148119c924b910288d10decd4cdbaeb9f536c
Your Name <you@example.com>
2023-03-05 02:29:10 +0530

    final commit

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let disable=document.querySelector('.disable')
 let blogs=document.querySelector('.blogs')
 
 function createBlog(){
+    let modal=document.createElement('div')
+    modal.classList.add('modal')
+    document.body.appendChild(modal)
 
     mainContainer.classList.add('main')
 
@@ -14,7 +17,7 @@ function createBlog(){
     head.classList.add('heading')
     head.textContent="Create A Post"
 
-    let blogheading=document.createElement('input')
+    let blogheading=document.createElement('textarea')
     blogheading.placeholder="Enter Your heading...."
     blogheading.classList.add('createheading')
 
@@ -52,6 +55,7 @@ function createBlog(){
     function closepopup(){
         newPost.classList.add('hidePopup')
         mainContainer.classList.remove('main')
+        modal.classList.remove('modal')
     }
 
     function publishPost(){
@@ -76,22 +80,27 @@ function createBlog(){
             deletes.textContent="Delete Post"
             deletes.classList.add('pbutton')
 
-            //let today=new Date().getTime
+            let today=new Date().toLocaleString()
+            let details=document.createElement('p')
+            details.classList.add('myTime')
+            details.innerText="Created At : " + today.substring(0,8) + " at" + today.substring(9,20)
 
             publishblock.appendChild(publishheading)
             publishblock.appendChild(publishpera)
             publishblock.appendChild(edit)
             publishblock.appendChild(deletes)
-            //publishblock.appendChild(today)
+            publishblock.appendChild(details)
             blogs.appendChild(publishblock)
             newPost.classList.add('hidePopup')
             mainContainer.classList.remove('main')
             
             function deleteblog(){
                 publishblock.remove()
+                modal.classList.remove('modal')
             }
 
             function editblog(){
+                modal.classList.add('modal')
                 mainContainer.classList.add('main')
 
                 let newPost_e=document.createElement('form')
@@ -101,12 +110,12 @@ function createBlog(){
                 head_e.classList.add('heading')
                 head_e.textContent="Edit A Post"
 
-                let blogheading_e=document.createElement('input')
+                let blogheading_e=document.createElement('textarea')
                 blogheading_e.textContent=publishheading.innerText
                 blogheading_e.classList.add('createheading')
 
                 let blogpera_e=document.createElement('textarea')
-                blogpera_e.textContent=blogpera.value
+                blogpera_e.textContent=publishpera.innerText
                 blogpera_e.classList.add('createpera')
 
                 let save=document.createElement('button')
@@ -127,6 +136,9 @@ function createBlog(){
                 close.textContent="Close"
                 close.type='button'
 
+                let today_e=new Date().toLocaleString()
+                details.innerText="Last Updated At : " + today_e.substring(0,8) + " at" + today_e.substring(9,20)
+
                 buttons.appendChild(save)
                 buttons.appendChild(delete2)
                 newPost_e.appendChild(head_e)
@@ -139,19 +151,38 @@ function createBlog(){
                 function closepopup2(){
                     newPost_e.classList.add('hidePopup')
                     mainContainer.classList.remove('main')
+                    modal.classList.remove('modal')
                 }
 
                 function deleteblog2(){
                     newPost_e.classList.add('hidePopup')
                     mainContainer.classList.remove('main')
                     publishblock.remove()
+                    modal.classList.remove('modal')
                 }
-                delete2.addEventListener('click',deleteblog2)
+
+                function saveblog(){
+
+                    if(blogheading_e.value!="" && blogpera_e.value!=""){
+                        publishheading.textContent=blogheading_e.value
+                        publishpera.textContent=blogpera_e.value
+                        
+                        newPost_e.classList.add('hidePopup')
+                        mainContainer.classList.remove('main')
+                        modal.classList.remove('modal')
+                    }else{
+                        alert("Fill all fields properly")
+                    }
+                }
+
                 close.addEventListener('click',closepopup2)
+                delete2.addEventListener('click',deleteblog2)
+                save.addEventListener('click',saveblog)
             }
 
             edit.addEventListener('click',editblog)
             deletes.addEventListener('click',deleteblog)
+            modal.classList.remove('modal')
         }else{
             alert("Fill all fields properly")
         }
